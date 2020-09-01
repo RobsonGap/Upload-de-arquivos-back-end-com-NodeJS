883202a2547aa5be13989d355ba4160a27e5acd7
RobsonGap <robsonmie@hotmail.com>
2020-09-01 11:02:04 -0300

    Importando o mongoose
    Mongoose e usado para importar e manipular dados no mongoDB, configurando setup de conexão com o mongodb.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const morgan = require("morgan");
+const mongoose = require('mongoose');
 
 const app = express();
+
+/**
+ * Database setup
+ */
+mongoose.connect('mongodb://localhost/noderest', {useCreateIndex: true, useUnifiedTopology: true,useNewUrlParser: true,useFindAndModify:false});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
